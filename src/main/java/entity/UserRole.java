caf5e256b4222169602c463daa12f531320b505c
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-12-17 05:10:20 +0300

    first

--- src/main/java/entity/UserRole.java
+++
@@ -1,7 +0,0 @@
-package entity;
-
-public enum UserRole {
-        ADMIN,
-        DOCTOR,
-        PATIENT;
-}
--- src/main/java/entity/UserState.java
+++
@@ -1,7 +0,0 @@
-package entity;
-
-public enum UserState {
-    ACTIVE,
-    INACTIVE,
-    BLOCKED;
-}
