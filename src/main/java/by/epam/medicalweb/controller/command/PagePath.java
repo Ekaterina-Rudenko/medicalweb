6ecc65c98339930448bc00f4f026494d1b78c68a
Ekaterina-Rudenko <katestasevich30@gmail.com>
2022-02-09 20:23:54 +0300

    Update PagePath.java

--- src/main/java/by/epam/medicalweb/controller/command/PagePath.java
+++ src/main/java/by/epam/medicalweb/controller/command/PagePath.java
@@ -9,5 +9,6 @@ public class PagePath {
     public static final String MAIN_PAGE = "jsp/pages/main.jsp";
     public static final String LOG_IN_PAGE = "jsp/pages/login.jsp";
     public static final String REGISTRATION_PAGE = "jsp/pages/registration.jsp";
+    public static final String SUCCESSFUL_REGISTRATION = "jsp/pages/successful_registration.jsp";
 
 }
